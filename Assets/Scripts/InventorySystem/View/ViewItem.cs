126c1e19fba1030057130f3f4b8b337c70f97618
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2023-09-27 12:14:25 +0300

    Implemented basic drag and drop functionality.

--- Assets/Scripts/InventorySystem/View/ViewItem.cs
+++ Assets/Scripts/InventorySystem/View/ViewItem.cs
@@ -1,9 +1,37 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace InventorySystem.View
 {
-    public class ViewItem : MonoBehaviour
+    public class ViewItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
+        private RectTransform _rectTransform;
+        private Canvas _mainCanvas;
+        private CanvasGroup _canvasGroup;
 
+        private void Start()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _mainCanvas = GetComponentInParent<Canvas>();
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            var slotTransfrom = _rectTransform.parent;
+            slotTransfrom.SetAsLastSibling();
+            _canvasGroup.blocksRaycasts = false;
+        }
+
+        public void OnDrag(PointerEventData eventData) 
+        {
+            _rectTransform.anchoredPosition += eventData.delta / _mainCanvas.scaleFactor;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            transform.localPosition = Vector3.zero;
+            _canvasGroup.blocksRaycasts = true;
+        }
     }
 }
--- Assets/Scripts/InventorySystem/View/ViewSlot.cs
+++ Assets/Scripts/InventorySystem/View/ViewSlot.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace InventorySystem.View
 {
-    public class ViewSlot : MonoBehaviour
+    public class ViewSlot : MonoBehaviour, IDropHandler
     {
-
+        public void OnDrop(PointerEventData eventData)
+        {
+            var otherItemTransform = eventData.pointerDrag.transform;
+            otherItemTransform.SetParent(transform);
+            otherItemTransform.localPosition = Vector3.zero;
+        }
     }
 }
