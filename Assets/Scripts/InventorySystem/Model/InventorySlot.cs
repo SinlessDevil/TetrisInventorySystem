47d2635db2f08aa09c247436514886def73409a4
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2023-09-27 11:42:01 +0300

    Implemented basic UI layout and reorganized folders for the MVC structure.

---
+++ Assets/Scripts/InventorySystem/Model/InventorySlot.cs
@@ -0,0 +1,32 @@
+using System;
+using InventorySystem.Abstract;
+
+namespace InventorySystem.Model
+{
+    public class InventorySlot : IInventorySlot
+    {
+        public bool IsFull => Amount == Capacity;
+        public bool IsEmpty => Item == null;
+        public IInventoryItem Item { get; private set; }
+        public Type ItemType => Item.Type;
+        public int Amount => IsEmpty ? 0 : Item.Amount;
+        public int Capacity { get; private set; }
+
+        public void SetItem(IInventoryItem item)
+        {
+            if (IsEmpty == false)
+                return;
+
+            this.Item = item;
+            this.Capacity = item.MaxItemsInInventorySlot;
+        }
+        public void Clear()
+        {
+            if (IsEmpty == true)
+                return;
+
+            Item.Amount = 0;
+            Item = null;
+        }
+    }
+}
---
+++ Assets/Scripts/InventorySystem/Model/Items/Apple.cs
@@ -0,0 +1,18 @@
+using System;
+using InventorySystem.Abstract;
+
+namespace InventorySystem.Model.Items
+{
+    public class Apple : IInventoryItem
+    {
+        public bool IsEquipped { get; set; }
+        public Type Type => GetType();
+
+        public int MaxItemsInInventorySlot { get; }
+        public int Amount { get; set; }
+
+        public Apple(int maxItemsInInventorySlot) => MaxItemsInInventorySlot = maxItemsInInventorySlot;
+
+        public IInventoryItem Clone() => new Apple(MaxItemsInInventorySlot);
+    }
+}
