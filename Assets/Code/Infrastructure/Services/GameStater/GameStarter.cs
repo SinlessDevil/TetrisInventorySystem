2e59938773b3b3c8c7f99eeb2f1539333327cf81
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-04-02 18:24:15 +0300

    Added debug

--- Assets/Code/Infrastructure/Services/GameStater/GameStarter.cs
+++ Assets/Code/Infrastructure/Services/GameStater/GameStarter.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Text;
 using Code.Infrastructure.Services.PersistenceProgress;
 using Code.Infrastructure.Services.PersistenceProgress.Player;
 using Code.Infrastructure.Services.SaveLoad;
 using Code.Infrastructure.Services.StaticData;
 using Code.Inventory.Services.InventoryExpand;
 using Code.InventoryModel;
+using Code.InventoryModel.Items.Data;
 using Code.InventoryModel.Services.InventoryPlayer;
 using Services.Factories.Inventory;
 using UnityEngine;
@@ -41,6 +44,8 @@ namespace Code.Infrastructure.Services.GameStater
             
             InitProgress();
             InitInventory();
+
+            DebugInventory();
         }
 
         private void InitInventory()
@@ -53,6 +58,30 @@ namespace Code.Infrastructure.Services.GameStater
         private void InitProgress()
         {
             _progressService.PlayerData = LoadProgress() ?? SetUpBaseProgress();   
+        }
+
+        private void DebugInventory()
+        {
+            TetrisInventoryData inventory = _progressService.PlayerData.InventoryData.PlayerInventory;
+            Debug.Log("===== Inventory State =====");
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int y = 0; y < inventory.Rows; y++)
+            {
+                string[] row = new string[inventory.Columns];
+
+                for (int x = 0; x < inventory.Columns; x++)
+                {
+                    GridCell cell = inventory.Cells[y * inventory.Columns + x];
+                    string cellContent = cell.Item != null ? cell.Item.Id.ToString() : " . ";
+                    row[x] = $"({x},{y}) {cellContent}";
+                }
+
+                sb.AppendLine(string.Join(" | ", row));
+            }
+
+            Debug.Log(sb.ToString());
         }
         
         private PlayerData LoadProgress()
@@ -75,7 +104,7 @@ namespace Code.Infrastructure.Services.GameStater
             _inventorySaveInitializer.Initialize(
                 initialItems: _staticDataService.Balance.Inventory.DefaultItems,
                 defaultBorders: _staticDataService.Balance.Inventory.DefaultOpenedCells);
-
+            
             return progress;
         }
     }
